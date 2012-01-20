2b19693e28c74a8d527cafadbee22870ef91bd74
evan <esims89@gmail.com>
2012-01-19 17:04:45 -0800

    removed tabs in asg1

--- asg1/code/jfmt.java
+++ asg1/code/jfmt.java
@@ -22,14 +22,14 @@ class jfmt {
    public static int exit_status = EXIT_SUCCESS;
    public static int width = 65;
    
-	public static void set_width(String[] args) {
-		try {
-			width = Integer.parseInt( args[0].substring(1) );
-		}catch (NumberFormatException error) {
-			out.printf ("NumberFormatException: %s%n",
-		            error.getMessage());
-		}
-	}
+        public static void set_width(String[] args) {
+                try {
+                        width = Integer.parseInt( args[0].substring(1) );
+                }catch (NumberFormatException error) {
+                        out.printf ("NumberFormatException: %s%n",
+                            error.getMessage());
+                }
+        }
 
    // A basename is the final component of a pathname.
    // If a java program is run from a jar, the classpath is the
@@ -43,45 +43,45 @@ class jfmt {
    
    // prints an array of words that comprise a paragraph.
    static void printParagraph(StringBuffer strBuf) {
-	   String str = strBuf.toString();
-	   str = str.replaceAll("\\s+\n", "\n");
-   	   str = str.replaceAll("\\s+$", "");
-	   out.printf( "%s%n", str );
+           String str = strBuf.toString();
+           str = str.replaceAll("\\s+\n", "\n");
+           str = str.replaceAll("\\s+$", "");
+           out.printf( "%s%n", str );
    }
 
    static void format (Scanner infile) {
-	   out.printf("%n");
-	   StringBuffer strBuf = new StringBuffer();
-	   int wcount = 0;
-	   boolean blankline = false;
-	   for (int linenr = 1; infile.hasNextLine (); ++ linenr) {
-		   String line = infile.nextLine ();
-		   List<String> words = new LinkedList<String> ();
-		   for (String word : line.split("\\s+")) {
-			   if (word.length () == 0) continue;
-			   words.add(word);
-		   }
-		   if (words.size() == 0) {
-			   if (blankline) { continue; }
-			   else {
-				   printParagraph(strBuf);
-				   out.printf("%n");
-				   blankline = true;
-				   strBuf = new StringBuffer();
-				   wcount = 0;
-			   }
-		   }
-		   else {
-		      for (String word : words) {
-				  // out.println("wcount == " + wcount);
-				  if (wcount + word.length() > width) { strBuf.append("\n"); wcount = 0; }
-				  strBuf.append(word + " ");
-	  			   blankline = false;
-				  wcount += word.length() + 1;
-			  }
-	       }
-	   }
-	   if (!blankline) printParagraph(strBuf);
+           out.printf("%n");
+           StringBuffer strBuf = new StringBuffer();
+           int wcount = 0;
+           boolean blankline = false;
+           for (int linenr = 1; infile.hasNextLine (); ++ linenr) {
+                   String line = infile.nextLine ();
+                   List<String> words = new LinkedList<String> ();
+                   for (String word : line.split("\\s+")) {
+                           if (word.length () == 0) continue;
+                           words.add(word);
+                   }
+                   if (words.size() == 0) {
+                           if (blankline) { continue; }
+                           else {
+                                   printParagraph(strBuf);
+                                   out.printf("%n");
+                                   blankline = true;
+                                   strBuf = new StringBuffer();
+                                   wcount = 0;
+                           }
+                   }
+                   else {
+                      for (String word : words) {
+                                  // out.println("wcount == " + wcount);
+                                  if (wcount + word.length() > width) { strBuf.append("\n"); wcount = 0; }
+                                  strBuf.append(word + " ");
+                                   blankline = false;
+                                  wcount += word.length() + 1;
+                          }
+               }
+           }
+           if (!blankline) printParagraph(strBuf);
    }
 
 
@@ -93,21 +93,21 @@ class jfmt {
          format (new Scanner (in));
       }else {
          // Check for width option
-		 if (args[0].startsWith("-")) {
-		    set_width(args);
-			if (args.length == 1) {
-	            // There are no filenames given on the command line.
-	            out.printf ("FILE: -%n");
-	            format (new Scanner (in));
-			}
-			else {
-			String[] tmp = new String[args.length-1];
-			for (int i = 0 ; i < tmp.length; ++i) tmp[i] = args[i+1];
-			args = tmp;
-			// out.print("width: " + width); // debug
-			}
-		 }
-		 
+                 if (args[0].startsWith("-")) {
+                    set_width(args);
+                        if (args.length == 1) {
+                    // There are no filenames given on the command line.
+                    out.printf ("FILE: -%n");
+                    format (new Scanner (in));
+                        }
+                        else {
+                        String[] tmp = new String[args.length-1];
+                        for (int i = 0 ; i < tmp.length; ++i) tmp[i] = args[i+1];
+                        args = tmp;
+                        // out.print("width: " + width); // debug
+                        }
+                 }
+                 
          // Iterate over each filename given on the command line.
          for (int argix = 0; argix < args.length; ++argix) {
             String filename = args[argix];
