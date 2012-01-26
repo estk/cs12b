b410ef3381b5314e04e706d53b3b0925be1d31f8
evan <esims89@gmail.com>
2012-01-25 16:25:21 -0800

    lab3 jrpn up to spec

--- lab3/jrpn.java
+++ lab3/jrpn.java
@@ -14,25 +14,14 @@ class jrpn {
         jrpn calc = new jrpn();
         while ( scan.hasNext() ) {
             String str = scan.next();
-            calc.push(str);
-            calc.show();
+            calc.evaluate(str);
         }
     }
     
-    // static Boolean isOperator(String str) {
-    //     if (str.length() != 0)
-    //     return (  str.equals("+")
-    //            || str.equals("-")
-    //            || str.equals("*")
-    //            || str.equals("/") );
-    //     // bahahah its almost like we're using haskell
-    //     // ... but we're not (frowny face).
-    // }
-
-    void push(String str) {
+    void evaluate(String str) {
         // try interpreting it as a double
         try {
-            this.concat(str);
+            push ( Double.parseDouble(str) );
             return;
         }
         catch (StackOverflowError e) {
@@ -44,17 +33,22 @@ class jrpn {
 
         // try interpreting it as an operator
         try {
-            this.operate(str);
+            operate(str);
         }
         catch (OperatorFormatException e) {
             err.println("The input was not recognized.");
         }
     }
     
-    void concat(String str) {
-        stack[top+1] = Double.parseDouble(str);
+    void push(double num) {
+        stack[top+1] = num;
         top += 1;
-        // out.printf("Got: %s%n", str); // debug
+    }
+
+    double pop() {
+        double res = stack[top];
+        top -= 1;
+        return res;
     }
 
     void operate(String str)
@@ -64,45 +58,39 @@ class jrpn {
         switch (c) {
             case '+': add();  break;
             case '-': sub();  break;
-            case '*': mul(); break;
+            case '*': mul();  break;
             case '/': div();  break;
+            case ';': show(); break;
             default: throw new OperatorFormatException();
         }
-        // out.printf("Got: %s%n", str); // debug
     }
 
     void add() {
-        double a = stack[top];
-        top -= 1;
-        double b = stack[top];
-        stack[top] = a + b;
+        double a = pop();
+        double b = pop();
+        push ( a + b );
     }
 
     void sub() {
-        double a = stack[top];
-        top -= 1;
-        double b = stack[top];
-        stack[top] = b - a;
+        double a = pop();
+        double b = pop();
+        push ( b - a );
     }
 
     void mul() {
-        double a = stack[top];
-        top -= 1;
-        double b = stack[top];
-        stack[top] = a * b;
+        double a = pop();
+        double b = pop();
+        push ( a * b );
     }
 
     void div() {
-        double a = stack[top];
-        top -= 1;
-        double b = stack[top];
-        stack[top] = b / a;
+        double a = pop();
+        double b = pop();
+        push ( b / a );
     }
 
     void show() {
-        out.print("Stack: ");
-        for (int i=0 ; i <= top ; i++) out.print(stack[i] + " ");
-        out.println();
+        for (int i=0 ; i <= top ; i++) out.printf( "%22.15g%n", stack[i] );
     }
 
     static class OperatorFormatException extends Exception {
